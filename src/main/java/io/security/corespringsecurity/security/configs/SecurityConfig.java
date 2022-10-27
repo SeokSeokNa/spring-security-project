5a1c00fe526bf5666264479d47a4077f789ce275
unknown <nsk7640@naver.com>
2022-10-27 22:02:01 +0900

    webignore config

--- src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -1,12 +1,14 @@
 package io.security.corespringsecurity.security.configs;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -38,13 +40,13 @@ public class SecurityConfig  {
         UserDetails sys = User.builder()
                 .username("manager")
                 .password(password)
-                .roles("MANAGER")
+                .roles("MANAGER" , "USER")
                 .build();
 
         UserDetails admin = User.builder()
                 .username("admin")
                 .password(password)
-                .roles("ADMIN")
+                .roles("ADMIN" , "USER" , "MANAGER")
                 .build();
 
         return new InMemoryUserDetailsManager(user, sys, admin);
@@ -68,5 +70,17 @@ public class SecurityConfig  {
                 .and()
                 .formLogin()
                 .and().build();
+    }
+
+    //webignore 설정(보안 필터를 거치지 않을 정적 자원들을 위한)
+    /*
+        atCommonLocations() 클릭해서 들어가면 -> StaticResourceLocation 클래스 내용안에 ignore 할 정적인 자원들의 path가 적혀있음
+        SecurityFilterChain 에 설정하지 않으면 webignore 설정으로 할 수 있음
+        차이점이라면 SecurityFilterChain에 설정할 경우 일단 보안필터를 거치긴 하나 permitAll 로 권한심사를 통과하여 모두에게 공개되는 것이고
+        webignore 설정은 보안필터 자체를 안거치게 한다.
+     */
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
